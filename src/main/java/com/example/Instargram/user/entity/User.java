717e4448a50beda811aa606b47344a1847ffce49
kangyonghyeon <khyeon001003@gmail.com>
2024-10-16 11:43:16 +0900

    [feat] Entity 수정

--- src/main/java/com/example/Instargram/user/entity/User.java
+++ src/main/java/com/example/Instargram/user/entity/User.java
@@ -1,23 +1,41 @@
 package com.example.Instargram.user.entity;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 import java.util.Date;
 
+
 @Data
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column
     String email;
+    @Column
     String password;
+    @Column
     String phoneNum;
+    @Column
     Boolean gender;
+    @Column
     Date birth;
+    @Column
     String name;
+    @Column
     String nickname;
+    @Column
     String webSite;
+    @Column
     String introduce;
+    @Column
     String imageUrl;
+
 
     @Builder
     public User(String email, String password, String phoneNum, Boolean gender, Date birth, String name, String nickname, String webSite, String introduce, String imageUrl) {
